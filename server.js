8cfb3a23d2e5c38591c9c1b0f57cde0f960d734d
Ravindu Balasuriya <ravindubalasuriya10@gmail.com>
2025-12-12 10:18:10 +0530

    simple web

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello! Meka mage mulma DevOps Project eka.');
+});
+
+app.listen(port, () => {
+  console.log(`App eka wada karanne methana: http://localhost:${port}`);
+});
